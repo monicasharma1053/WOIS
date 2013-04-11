56e56573646818b5d60621da7139db2e9cda742c
Monica Sharma <monicasharma1053@gmail.com>
2013-04-11 18:04:00 -0500

    files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WOIS::Application.config.secret_token = '7069b09f5bf3a547557d4f501f7a48d5e6010d28b75454a41f034e8311a26d65a6233fd0896412c16da29d5867127f49207f36366693e0c396f72fb5599dd177'
