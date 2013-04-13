35d02dd8162e90135b73be9b836d7d5affd951f9
Monica Sharma <monicasharma1053@gmail.com>
2013-04-13 12:13:18 -0500

    titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def full_title(page_title)
+		base_title = "We Own It Summit 2013"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end	
 end
